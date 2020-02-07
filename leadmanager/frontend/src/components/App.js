d6815c1f3fb9fa4d9fff7ab83ffc199f25e01957
dlauck92 <drewlauck92@gmail.com>
2020-02-07 15:24:29 -0500

    adding stores

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './layout/Header';
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
+
+import { Provider } from "react-redux";
+import store from "../store";
 
 class App extends Component {
-    render() {
-        return (
-            <Header />
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </Fragment>
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
